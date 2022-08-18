184510bfa7d5a85b4fd7da617c03cbda0adbdf6e
tronurg <onurgultekin@live.com>
2022-08-18 22:09:39 +0300

    small update

--- dms-commons/src/com/ogya/dms/commons/structures/StatusInfo.java
+++ dms-commons/src/com/ogya/dms/commons/structures/StatusInfo.java
@@ -1,9 +1,14 @@
 package com.ogya.dms.commons.structures;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class StatusInfo {
 
+	@JsonProperty("a")
 	public String address;
+	@JsonProperty("b")
 	public int messageNumber;
+	@JsonProperty("c")
 	public int progress;
 
 	public StatusInfo() {
